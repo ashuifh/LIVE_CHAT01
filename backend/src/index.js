9a389f7db8d0ce5bbd83d644945c9635558d3bcd
ashuifh <111656275+ashuifh@users.noreply.github.com>
2025-07-01 15:46:40 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,12 +31,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+
 
 server.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);

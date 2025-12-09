a3e49bf2c681ad92647c222f633b7519098ef818
Ayush kalakoti <111656275+ashuifh@users.noreply.github.com>
2025-12-09 23:38:50 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://live-chat01-6.onrender.com",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "https://live-chat01-6.onrender.com/api",
   withCredentials: true,
 });

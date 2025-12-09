897cf26d4105d2f3b4140bbbfcc48b2e478a7af9
Ayush kalakoti <111656275+ashuifh@users.noreply.github.com>
2025-12-09 18:10:07 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,12 +19,17 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5175"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5175",
+      "https://live-chat-01-hlld.vercel.app",  // add this
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 app.get("/", (req, res) => {
   res.status(200).json({
